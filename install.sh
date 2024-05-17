ac060f20340ce803f8996bd19005ca4800eb0e7c
stupid cat <cat@blargbot.xyz>
2024-05-17 13:45:10 -0600

    add requirements.txt

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 chmod +x ./monitor.py
+python -m pip install -r requirements.txt
 cp screenshot-monitor.service /home/deck/.config/systemd/user
 systemctl --user daemon-reload
 systemctl --user enable screenshot-monitor.service
